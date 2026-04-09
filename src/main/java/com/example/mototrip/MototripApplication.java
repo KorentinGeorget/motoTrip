103035b7fc73686241ff04ae1c19af4c7fb3efcd
KorentinGeorget <korentin.georget@gmail.com>
2026-04-09 13:33:07 +0200

    🎉 Setup

---
+++ src/main/java/com/example/mototrip/MototripApplication.java
@@ -0,0 +1,13 @@
+package com.example.mototrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MototripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MototripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mototrip/MototripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mototrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MototripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
